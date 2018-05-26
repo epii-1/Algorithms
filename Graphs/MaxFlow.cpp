31b8bbb9ad95335c0fc9ea50aecc9a9d81edeeb4
leier318 <leier318@student.liu.se>
2018-05-26 22:39:45 +0200

    Updated maxflow to class

--- Graphs/MaxFlow.cpp
+++ Graphs/MaxFlow.cpp
@@ -23,8 +23,7 @@ using namespace std;
 
 //https://www.geeksforgeeks.org/fast-io-for-competitive-programming/
 template<typename T>
-void fastScan(T &number)
-{
+void fastScan(T &number) {
     //variable to indicate sign of input number
     bool negative = false;
     register T c;
@@ -56,79 +55,109 @@ void fastScan(T &number)
         number *= -1;
 }
 
-struct Edge {
-    Edge(int u, int v, int c, int rev) : u(u), v(v), c(c), rev(rev), flow{ 0 } {}
-    int u, v, c, rev;
-    long long flow;
-};
 
-struct Node {
-    vector<Edge> edges;
-    vector<int> reverse;
-};
+class MaxFlowDinics {
+public:
 
-//Dinic's Algorithm
-long long send(int u, long long flow, int t, vector<Node> & nodes, vector<int> & currEdge, vector<int> & level) {
-    if (u == t)
-        return flow;
-    int size(nodes[u].edges.size());
-    long long currFlow, tmpFlow, currCurrEdge;
-    Edge *e;
-    for (; currEdge[u] < size; ++currEdge[u]) {
-        e = &nodes[u].edges[currEdge[u]];
-        if (level[e->v] == level[u] + 1 && e->flow < e->c) {
-            currFlow = min(flow, e->c - e->flow);
-            currCurrEdge = currEdge[u];
-            tmpFlow = send(e->v, currFlow, t, nodes, currEdge, level);
-            if (tmpFlow > 0) {
-                e->flow += tmpFlow;
-                nodes[e->v].edges[nodes[u].reverse[currCurrEdge]].flow -= tmpFlow;
-                return tmpFlow;
+    MaxFlowDinics(size_t _V) : _V(_V), _nodes(_V) {}
+
+    void add(int from, int to, int capacity) {
+        _nodes[from].edges.emplace_back(from, to, capacity, _nodes[to].edges.size());
+        _nodes[to].edges.emplace_back(to, from, 0, _nodes[from].edges.size() - 1);
+    }
+
+    void reset() {
+        for (size_t i{ 0 }, u, size; i < _V; ++i) {
+            size = _nodes[i].edges.size();
+            for (u = 0; u < size; ++u)
+                _nodes[i].edges[u].flow = 0;
+        }
+    }
+
+    //nput: A network{ \displaystyle G = ((V,E),c,s,t) } G = ((V, E), c, s, t).
+    //Output : An{ \displaystyle s - t } s - t flow{ \displaystyle f } f of maximum value.
+    long long solve(int s, int t) {
+        //Set f(e) = 0 for each e in E.
+        //Construct G_L from G_f of G. If dist(t) = infty stop and output f.
+        int u, size, i, totalFlow{ 0 }, flow;
+        vector<size_t> v(_V);
+        vector<int> level(_V);
+        while (true) {
+            fill(level.begin(), level.end(), -1);
+            //BFS
+            level[s] = 0;  // Level of source vertex
+            queue<int> q;
+            q.push(s);
+            while (!q.empty()) {
+                u = q.front(), q.pop();
+                size = _nodes[u].edges.size();
+                for (i = 0; i < size; ++i) {
+                    Edge &e(_nodes[u].edges[i]);
+                    if (level[e.v] == -1 && e.flow < e.c) {
+                        level[e.v] = level[u] + 1;
+                        q.push(e.v);
+                    }
+                }
+            }
+            if (level[t] == -1)
+                return totalFlow;
+
+            fill(v.begin(), v.end(), 0);
+            //Find a blocking flow f' in G_L.
+            flow = send(s, std::numeric_limits<long long>::max(), t, v, level);
+            while (flow) {
+                totalFlow += flow;
+                flow = send(s, std::numeric_limits<long long>::max(), t, v, level);
             }
         }
     }
-    return 0;
-}
+    //private:
+    struct Edge {
+        Edge(int u, int v, int c, int rev) : u(u), v(v), c(c), rev(rev), flow{ 0 } {}
+        int u, v, c, rev;
+        long long flow;
+    };
 
-//nput: A network{ \displaystyle G = ((V,E),c,s,t) } G = ((V, E), c, s, t).
-//Output : An{ \displaystyle s - t } s - t flow{ \displaystyle f } f of maximum value.
-long long maxFlowDinics(int s, int t, size_t V, vector<Node> & nodes) {
-    //Set f(e) = 0 for each e in E.
-    //Construct G_L from G_f of G. If dist(t) = infty stop and output f.
-    int u, size, i, totalFlow{ 0 }, flow;
-    vector<int> v(V);
-    vector<int> level(V);
-    while (true) {
-        fill(level.begin(), level.end(), -1);
-        //BFS
-        level[s] = 0;  // Level of source vertex
-        queue<int> q;
-        q.push(s);
-        while (!q.empty()) {
-            u = q.front(), q.pop();
-            size = nodes[u].edges.size();
-            for (i = 0; i < size; ++i) {
-                Edge &e(nodes[u].edges[i]);
-                if (level[e.v] == -1 && e.flow < e.c) {
-                    level[e.v] = level[u] + 1;
-                    q.push(e.v);
+    struct Node {
+        vector<Edge> edges;
+        vector<Edge*> reverse;
+    };
+
+    //Dinic's Algorithm
+    long long send(int u, long long flow, int t, vector<size_t> & currEdge, vector<int> & level) {
+        if (u == t)
+            return flow;
+        Node* n{ &_nodes[u] };
+        vector<Edge>* nue{ &n->edges };
+        size_t size(nue->size());
+        long long currFlow, tmpFlow, currCurrEdge;
+        Edge *e;
+        if (!n->reverse.size()) {
+            for (size_t i{ 0 }; i < size; ++i) {
+                e = &nue->at(i);
+                n->reverse.push_back(&_nodes[e->v].edges[e->rev]);
+            }
+        }
+
+        for (; currEdge[u] < size; ++currEdge[u]) {
+            e = &nue->at(currEdge[u]);
+            if (level[e->v] == level[u] + 1 && e->flow < e->c) {
+                currFlow = min(flow, e->c - e->flow);
+                currCurrEdge = currEdge[u];
+                tmpFlow = send(e->v, currFlow, t, currEdge, level);
+                if (!tmpFlow) {
+                    e->flow += tmpFlow;
+                    n->reverse[currCurrEdge]->flow -= tmpFlow;
+                    return tmpFlow;
                 }
             }
         }
-        if (level[t] == -1)
-            return totalFlow;
-
-        fill(v.begin(), v.end(), 0);
-        //Find a blocking flow f' in G_L.
-        flow = send(s, std::numeric_limits<long long>::max(), t, nodes, v, level);
-        while (flow) {
-            totalFlow += flow;
-            flow = send(s, std::numeric_limits<long long>::max(), t, nodes, v, level);
-        }
+        return 0;
     }
 
-}
-
+    size_t _V;
+    vector<Node> _nodes;
+};
 
 int main() {
     ios::sync_with_stdio(false);
@@ -144,7 +173,7 @@ int main() {
     fastScan(t);
 
 
-    vector<Node> nodes(n);
+    MaxFlowDinics maxFlow(n);
 
     ++m;
     while (--m) {
@@ -152,34 +181,29 @@ int main() {
         fastScan(t1);
         fastScan(t2);
         fastScan(t3);
-        nodes[t1].edges.emplace_back(t1, t2, t3, nodes[t2].reverse.size());
-        nodes[t2].edges.emplace_back(t2, t1, 0, nodes[t1].reverse.size());
-        nodes[t1].reverse.push_back(nodes[t2].edges.size() - 1);
-        nodes[t2].reverse.push_back(nodes[t1].edges.size() - 1);
+        maxFlow.add(t1, t2, t3);
     }
 
-    long long mflow(maxFlowDinics(s, t, n, nodes));
+    long long mflow(maxFlow.solve(s, t));
 
     vector<int> a;
 
-    for (i = 0; i < nodes.size(); ++i) {
-        for (j = 0; j < nodes[i].edges.size(); ++j) {
-            if (nodes[i].edges[j].c > 0 && nodes[i].edges[j].flow > 0) {
-                a.push_back(nodes[i].edges[j].u);
-                a.push_back(nodes[i].edges[j].v);
-                a.push_back(nodes[i].edges[j].flow);
+    for (i = 0; i < maxFlow._nodes.size(); ++i) {
+        for (j = 0; j < maxFlow._nodes[i].edges.size(); ++j) {
+            if (maxFlow._nodes[i].edges[j].c > 0 && maxFlow._nodes[i].edges[j].flow > 0) {
+                a.push_back(maxFlow._nodes[i].edges[j].u);
+                a.push_back(maxFlow._nodes[i].edges[j].v);
+                a.push_back(maxFlow._nodes[i].edges[j].flow);
             }
         }
     }
 
     printf("%d %lld %zu\n", n, mflow, a.size() / 3);
 
-    for (i = 0; i < a.size(); i += 3) {
+    for (i = 0; i < a.size(); i += 3)
         printf("%d %d %d\n", a[i], a[i + 1], a[i + 2]);
-    }
 
-    //}
-
+    system("Pause");
     return 0;
 }
 
