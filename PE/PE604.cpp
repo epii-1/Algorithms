67c417beeb2b455636d62bdebc1012062ef8a838
epii-1 <35694212+epii-1@users.noreply.github.com>
2018-06-02 21:48:00 +0200

    Update PE604.cpp

--- PE/PE604.cpp
+++ PE/PE604.cpp
@@ -1,4 +1,4 @@
-/* Let F(N)F(N) be the maximum number of lattice points in an axis - aligned N×NN×N square that the graph of a single strictly convex increasing function can pass through.
+/* Let F(N)F(N) be the maximum number of lattice points in an axis - aligned NÃ—NNÃ—N square that the graph of a single strictly convex increasing function can pass through.
 
 You are given that F(1) = 2F(1) = 2, F(3) = 3F(3) = 3, F(9) = 6F(9) = 6, F(11) = 7F(11) = 7, F(100) = 30F(100) = 30 and F(50000) = 1898F(50000) = 1898.
 Below is the graph of a function reaching the maximum 3 for N = 3N = 3:
@@ -6,7 +6,6 @@ Below is the graph of a function reaching the maximum 3 for N = 3N = 3:
 p604_convex3.png
 Find F(1018). 
 
-1398582231101
 */
 
 #include <iostream>
