1dab0385fdccb9ed3dd5706152a72f3f30c8323b
Leif H. Eriksson <leif.h.eriksson@se.abb.com>
2018-07-19 10:53:59 +0200

    Template EP

--- Graphs/EulerPath.cpp
+++ Graphs/EulerPath.cpp
@@ -21,167 +21,189 @@
 
 using namespace std;
 
+#define _UNLOCKED 0
+#if _UNLOCKED
+#define gc() getchar_unlocked()
+#else
+#define gc() getchar()
+#endif
+
 //https://www.geeksforgeeks.org/fast-io-for-competitive-programming/
-template<typename T>
-void fastScan(T &number) {
-    register T c;
+template<typename C>
+void fastScan(C &number) {
+	register C c;
 
-    number = 0;
+	number = 0;
 
-    // extract current character from buffer
-    c = getchar_unlocked();
-    while (!(c > 47 && c < 58))
-        c = getchar_unlocked();
+	// extract current character from buffer
+	c = gc();
+	while (!(c > 47 && c < 58))
+		c = gc();
 
-
-    // Keep on extracting characters if they are integers
-    // i.e ASCII Value lies from '0'(48) to '9' (57)
-    for (; (c > 47 && c < 58); c = getchar_unlocked())
-        number = number * 10 + c - 48;
+	// Keep on extracting characters if they are integers
+	// i.e ASCII Value lies from '0'(48) to '9' (57)
+	for (; (c>47 && c<58); c = gc())
+		number = number * 10 + c - 48;
 }
 
-struct Node {
-    vector<int> edges;
-    int i{ 0 }, to{ 0 }, from{ 0 };
+template <typename T = int>
+class EulerianPath {
+private:
+	struct _Node {
+		vector<T> edges;
+		T i{ 0 }, to{ 0 }, from{ 0 };
+	};
+
+public:
+	vector<_Node> nodes;
+
+	EulerianPath() {}
+
+	void resize(size_t n) {
+		fill(nodes.begin(), min(nodes.end(), nodes.begin() + n), _Node());
+		nodes.resize(n);
+	}
+
+	bool eulerianPath(list<T> & l) {
+		T s{ 0 }, i;
+
+		//Find a logical startnode
+		//(Should add a check to only do this is oddIn,
+		//Else it should only make sure first node got from > 0)
+		for (i = 0; i < nodes.size(); ++i) {
+			if (nodes[i].from > nodes[i].to) {
+				s = i;
+				break;
+			}
+			//if(nodes[i].from > 0) s = i; //Read comment down bellow
+		}
+
+		//Interesting. Is this really correct? What if we have a full cycle without node 0?
+
+		//Walk from start (or node 0)
+		_eulerianPathDeep(s, l, -1, -1);
+		l.push_front(s);
+
+		//Recheck
+		if (!_recheck(l))
+			return false;
+
+		//Find unvisited nodes
+		for (i = 0; i < nodes.size(); ++i)
+			if (nodes[i].i < nodes[i].from)
+				return false;
+
+		return true;
+	}
+
+private:
+
+	bool _eulerianPathDeep(T i, list<T> & l, const T e, const T e2) {
+		_Node* curr{ &nodes[i] };
+		//DF walk untill we get stuck
+		while (true) {
+			_Node* next{ &(nodes[curr->edges[curr->i]]) };
+			i = curr->edges[curr->i];
+
+			++curr->i;
+			if (i == e) //We found a cycle, can return it
+				return true;
+
+			l.push_back(i); //Add to path
+
+			if (i == e2) //We found the start, we can return it
+				return false;
+
+			if (next->i == next->from) //This is bad
+				return false; //Next is already full
+
+			curr = next;
+		}
+	}
+
+	//Recheck everything along to path to se if some node still have unused edges
+	bool _recheck(list<T> & l) {
+		auto it{ l.begin() };
+		for (; it != l.end(); ++it) {
+			while (nodes[*it].i < nodes[*it].from) {
+				list<T> l2;
+				if (_eulerianPathDeep(*it, l2, *it, *l.begin())) {
+					//Add the newfound cycle
+					l2.push_front(*it);
+					it = l.insert(it, l2.begin(), l2.end());
+				}
+				else //Bad Path
+					return false;
+			}
+		}
+		return true;
+	}
+
+
 };
-
-bool eulerianPathDeep(vector<Node> & nodes, int i, list<int> & l, const int e, const int e2) {
-    Node* curr{ &nodes[i] };
-    //DF walk untill we get stuck
-    while (true) {
-        Node* next{ &(nodes[curr->edges[curr->i]]) };
-        i = curr->edges[curr->i];
-
-        ++curr->i;
-        if (i == e) //We found a cycle, can return it
-            return true;
-
-        l.push_back(i); //Add to path
-
-        if (i == e2) //We found the start, we can return it
-            return false;
-
-        if (next->i == next->from) //This is bad
-            return false; //Next is already full
-
-        curr = next;
-    }
-}
-
-//Recheck everything along to path to se if some node still have unused edges
-bool recheck(vector<Node> & nodes, list<int> & l) {
-    auto it{ l.begin() };
-    for (; it != l.end(); ++it) {
-        while (nodes[*it].i < nodes[*it].from) {
-            list<int> l2;
-            if (eulerianPathDeep(nodes, *it, l2, *it, *l.begin())) {
-                //Add the newfound cycle
-                l2.push_front(*it);
-                it = l.insert(it, l2.begin(), l2.end());
-            }
-            else //Bad Path
-                return false;
-
-        }
-    }
-    return true;
-}
-
-bool eulerianPath(vector<Node> & nodes, list<int> & l) {
-    int s{ 0 }, i;
-
-    //Find a logical startnode
-    //(Should add a check to only do this is oddIn,
-    //Else it should only make sure first node got from > 0)
-    for (i = 0; i < nodes.size(); ++i) {
-        if (nodes[i].from > nodes[i].to) {
-            s = i;
-            break;
-        }
-        //if(nodes[i].from > 0) s = i; //Read comment down bellow
-    }
-
-    //Interesting. Is this really correct? What if we have a full cycle without node 0?
-
-    //Walk from start (or node 0)
-    eulerianPathDeep(nodes, s, l, -1, -1);
-    l.push_front(s);
-
-    //Recheck
-    if (!recheck(nodes, l))
-        return false;
-
-    //Find unvisited nodes
-    for (i = 0; i < nodes.size(); ++i)
-        if (nodes[i].i < nodes[i].from)
-            return false;
-
-    return true;
-}
 
 
 int main() {
-    ios::sync_with_stdio(false);
-    cout.tie(nullptr);
-    cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
 
-    int n, m, i, j, t1, t2;
-    int m2;
-    int oddOut{ 0 }, oddIn{ 0 };
-    vector<Node> nodes;
-    while (true) {
-        //input blablabla
-        fastScan(n);
-        fastScan(m);
+	int n, m, i, j, t1, t2;
+	int m2;
+	int oddOut{ 0 }, oddIn{ 0 };
+	EulerianPath<int> ep;
+	while (true) {
+		//input blablabla
+		fastScan(n);
+		fastScan(m);
 
-        if (n == 0 && m == 0)
-            return 0;
+		if (n == 0 && m == 0)
+			return 0;
 
-        //nodes.clear();
-        fill(nodes.begin(), min(nodes.end(), nodes.begin() + n), Node());
-        nodes.resize(n);
-        m2 = m;
-        ++m;
-        oddOut = oddIn = 0;
-        while (--m) {
-            //input blablabla
-            fastScan(t1);
-            fastScan(t2);
-            nodes[t1].edges.emplace_back(t2);
-            ++nodes[t1].from;
-            ++nodes[t2].to;
+		ep.resize(n);
 
-            //Keeping count on odds nodes
-            /*if (t1 != t2) {
-            if (nodes[t1].from == nodes[t1].to + 1)
-            ++oddOut;
-            else if (nodes[t1].from == nodes[t1].to)
-            --oddIn;
-            if (nodes[t2].from + 1 == nodes[t2].to)
-            ++oddIn;
-            else if (nodes[t2].from == nodes[t2].to)
-            --oddOut;
-            }*/
-        }
+		m2 = m;
+		++m;
+		oddOut = oddIn = 0;
+		while (--m) {
+			//input blablabla
+			fastScan(t1);
+			fastScan(t2);
+			ep.nodes[t1].edges.emplace_back(t2);
+			++ep.nodes[t1].from;
+			++ep.nodes[t2].to;
 
-        //If there are more then 1 start, or more then 1 end,
-        //Or 1 end without start
-        //Or 1 start without end
-        //We cant find a E-path
-        //(Or if we find 2 disjoint cycles)
-        list<int> l;
-        if (oddIn > 1 || oddOut > 1 || oddIn != oddOut || !eulerianPath(nodes, l))
-            cout << "Impossible\n";
+			//Keeping count on odds nodes
+			/*if (t1 != t2) {
+			if (nodes[t1].from == nodes[t1].to + 1)
+			++oddOut;
+			else if (nodes[t1].from == nodes[t1].to)
+			--oddIn;
+			if (nodes[t2].from + 1 == nodes[t2].to)
+			++oddIn;
+			else if (nodes[t2].from == nodes[t2].to)
+			--oddOut;
+			}*/
+		}
 
-        else {
-            copy(l.begin(), l.end(), std::ostream_iterator<int>(cout, " "));
+		//If there are more then 1 start, or more then 1 end,
+		//Or 1 end without start
+		//Or 1 start without end
+		//We cant find a E-path
+		//(Or if we find 2 disjoint cycles)
+		list<int> l;
+		if (oddIn > 1 || oddOut > 1 || oddIn != oddOut || !ep.eulerianPath(l))
+			cout << "Impossible\n";
 
-            cout << "\n";
-        }
-    }
+		else {
+			copy(l.begin(), l.end(), std::ostream_iterator<int>(cout, " "));
+
+			cout << "\n";
+		}
+	}
 
 
-    return 0;
+	return 0;
 }
 /*
 2 3
